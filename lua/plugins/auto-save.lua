0728cf28e1bac0e5f0772d498fd2a30fc776258d
Michal Stolarczyk <zerodivisible@gmail.com>
2024-03-14 16:37:41 -0600

    fixing config

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,11 +1,14 @@
--- return {
---   "Pocco81/auto-save.nvim",
---   opts = {
---     enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
---     execution_message = {
---       message = nil,
---     },
---     debounce_delay = 9000, -- saves the file at most every `debounce_delay` milliseconds
---   },
--- }
-return {}
+if true then
+  return {}
+end
+
+return {
+  "Pocco81/auto-save.nvim",
+  opts = {
+    enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    execution_message = {
+      message = nil,
+    },
+    debounce_delay = 9000, -- saves the file at most every `debounce_delay` milliseconds
+  },
+}
