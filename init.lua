1c9e930f5e08f3dfac5577f071ea4c093422f8b7
Michal Stolarczyk <michal.stolarczyk@workday.com>
2024-04-17 21:51:43 -0600

    updates

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ require("config.lazy")
 
 -- use a gui font when running in gui
 if vim.g.neovide then
-  vim.opt.guifont = { "Berkeley Mono", ":h18" }
+  vim.opt.guifont = { "Berkeley Mono", ":h20" }
 end
