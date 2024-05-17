e8ccb7498821d57145745eff0cdb753b130e4588
Michal Stolarczyk <michal.stolarczyk@workday.com>
2024-05-17 08:42:23 -0600

    Updating dependencies for latest release of neovim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,7 +33,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.coding.native_snippets" },
     { import = "plugins" },
   },
 
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -19,6 +19,7 @@ return {
       "yaml",
       "ruby",
       "go",
+      "cpp",
       "rust",
       "jsonnet",
     })
